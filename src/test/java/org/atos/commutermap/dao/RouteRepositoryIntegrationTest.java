f257209001b8164d433ee9cd72f08b762a359b49
atos <atoska88@gmail.com>
2019-11-09 14:17:39 +0100

    Rollback transactions after tests

--- src/test/java/org/atos/commutermap/dao/RouteRepositoryIntegrationTest.java
+++ src/test/java/org/atos/commutermap/dao/RouteRepositoryIntegrationTest.java
@@ -9,6 +9,7 @@ import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -23,6 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ContextConfiguration(classes = {
         DatabaseConfig.class
 })
+@Rollback
 class RouteRepositoryIntegrationTest {
 
     @Autowired
