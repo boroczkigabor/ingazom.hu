eee0c292f6311e2262904a2ed0d7e2182c7fad73
atos <atoska88@gmail.com>
2019-11-12 21:32:31 +0100

    Fixed Elvira Url + Use next Monday for Elvira page load

--- src/main/java/org/atos/commutermap/network/service/ElviraUrlCreator.java
+++ src/main/java/org/atos/commutermap/network/service/ElviraUrlCreator.java
@@ -10,6 +10,8 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static java.time.DayOfWeek.MONDAY;
+
 @Component
 public class ElviraUrlCreator {
 
@@ -20,13 +22,17 @@ public class ElviraUrlCreator {
     * http://elvira.mav-start.hu/elvira.dll/x/uf?_charset_=UTF-8&i=BUDAPEST*&e=Magl%C3%B3d&v=&d=19.11.09
     */
     public String createElviraUrlFor(Route route) {
+        LocalDate today = LocalDate.now();
+        LocalDate nextMonday = today.minusDays(today.getDayOfWeek().getValue() - MONDAY.getValue())
+                .plusWeeks(1);
         return UriComponentsBuilder.fromHttpUrl(elviraBaseUrl)
                 .pathSegment("x")
                 .pathSegment("uf")
                 .queryParam("_charset_", StandardCharsets.UTF_8.displayName())
+                .queryParam("mikor", "-1")
                 .queryParam("i", UrlEscapers.urlFragmentEscaper().escape(route.departureStation.name))
                 .queryParam("e", UrlEscapers.urlFragmentEscaper().escape(route.destinationStation.name))
-                .queryParam("d", LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd")))
+                .queryParam("d", nextMonday.format(DateTimeFormatter.ofPattern("yy.MM.dd")))
                 .build(true)
                 .toUriString();
     }
