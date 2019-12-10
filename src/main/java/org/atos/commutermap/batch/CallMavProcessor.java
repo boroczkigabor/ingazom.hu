e766c6e5eadc0ae3e0fb1e2783d5bd43c83aec5a
atos <atoska88@gmail.com>
2019-12-10 23:07:26 +0100

    Smarter error handling

--- src/main/java/org/atos/commutermap/batch/CallMavProcessor.java
+++ src/main/java/org/atos/commutermap/batch/CallMavProcessor.java
@@ -2,16 +2,20 @@ package org.atos.commutermap.batch;
 
 import com.google.common.collect.ImmutableSortedSet;
 import org.atos.commutermap.dao.model.Route;
+import org.atos.commutermap.network.model.ErrorMessage;
 import org.atos.commutermap.network.model.TravelOffer;
 import org.atos.commutermap.network.model.TravelOfferRequest;
 import org.atos.commutermap.network.model.TravelOfferResponse;
 import org.atos.commutermap.network.service.MavinfoServerCaller;
+import org.hibernate.jdbc.BatchFailedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 
 import java.time.LocalDateTime;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class CallMavProcessor implements ItemProcessor<TravelOfferRequest, Route> {
 
@@ -28,16 +32,10 @@ public class CallMavProcessor implements ItemProcessor<TravelOfferRequest, Route
         LOGGER.info("Calling MAV for {} - {}.", request.departure.name, request.destination.name);
         TravelOfferResponse response = serverCaller.callServerWith(request);
         if (!response.errorMessages.isEmpty() || response.travelOffers.isEmpty()) {
-            boolean exhausted = response.errorMessages
-                    .stream()
-                    .anyMatch(msg -> "0".equals(msg.ID));
-            if (exhausted) {
-                LOGGER.warn("MAV server got exhausted, waiting 30 seconds...");
-                Util.sleep(30_000);
+            ErrorType errorType = ErrorType.fromErrorMessages(response.errorMessages);
+            errorType.handleError();
+            if (ErrorType.EXHAUSTED.equals(errorType)) {
                 return process(request);
-            } else {
-                LOGGER.info("Skipping {} - {} as response is empty.", request.departure.name, request.destination.name);
-                return null;
             }
         }
         ImmutableSortedSet<TravelOffer> sortedOffers = ImmutableSortedSet
@@ -48,5 +46,53 @@ public class CallMavProcessor implements ItemProcessor<TravelOfferRequest, Route
         TravelOffer firstOffer = sortedOffers.first();
 
         return new Route(request.departure, request.destination, firstOffer.details.realDepartureStation, firstOffer.price, firstOffer.travelTime, firstOffer.distance, LocalDateTime.now());
+    }
+
+    private enum ErrorType {
+        EXHAUSTED {
+            @Override
+            void handleError() {
+                LOGGER.warn("MAV server got exhausted, waiting 30 seconds...");
+                Util.sleep(30_000);
+            }
+        },
+        OUTDATED_VERSION {
+            @Override
+            void handleError() {
+                LOGGER.error("MAV server integration is outdated, update required!");
+                throw new BatchFailedException("MAV server integration is outdated, update required!");
+            }
+        },
+        MISSING_PARAMETER {
+            @Override
+            void handleError() {
+                OUTDATED_VERSION.handleError();
+            }
+        },
+        UNKNOWN {
+            @Override
+            void handleError() {
+
+            }
+        };
+
+        abstract void handleError();
+
+        static ErrorType fromErrorMessages(List<ErrorMessage> errorMessages) {
+            Stream<ErrorMessage> stream = errorMessages.stream();
+            if (hasErrorCode(stream, "0")) {
+                return EXHAUSTED;
+            } else if (hasErrorCode(stream, "460")) {
+                return OUTDATED_VERSION;
+            } else if (hasErrorCode(stream, "100")) {
+                return MISSING_PARAMETER;
+            } else {
+                return UNKNOWN;
+            }
+        }
+
+        private static boolean hasErrorCode(Stream<ErrorMessage> stream, String errorCode) {
+            return stream.anyMatch(msg -> errorCode.equals(msg.ID));
+        }
     }
 }
