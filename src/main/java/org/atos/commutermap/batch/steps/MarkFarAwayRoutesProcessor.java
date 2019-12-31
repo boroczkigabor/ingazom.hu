8756db1e6844b75aba4ae5bab821d02d8257cbe3
atos <atoska88@gmail.com>
2019-12-31 10:38:32 +0100

    Fix typo

--- src/main/java/org/atos/commutermap/batch/steps/MarkFarAwayRoutesProcessor.java
+++ src/main/java/org/atos/commutermap/batch/steps/MarkFarAwayRoutesProcessor.java
@@ -1,5 +1,6 @@
 package org.atos.commutermap.batch.steps;
 
+import org.atos.commutermap.batch.Util;
 import org.atos.commutermap.dao.model.Route;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
@@ -9,7 +10,7 @@ public class MarkFarAwayRoutesProcessor extends StepExecutionAware implements It
 
     @Override
     public Route process(Route item) {
-        Long maxDurationInMinutes = stepExecution.getJobExecution().getJobParameters().getLong("filter.routes.longer.than");
+        Long maxDurationInMinutes = stepExecution.getJobExecution().getJobParameters().getLong(Util.FILTER_LONGER_THAN_KEY);
         if (item.duration == null || item.duration.toMinutes() > maxDurationInMinutes) {
             LoggerFactory.getLogger(getClass()).info("Marking {} as far away because duration is greater than maximum: {} > {}", item.destinationStation.name, item.duration == null ? null : item.duration.toMinutes(), maxDurationInMinutes);
             item.markFarAway();
