ace1f48d5f0f4a6760d9612667bc5f62a868b304
atos <atoska88@gmail.com>
2019-12-31 12:43:06 +0100

    Config endpoint kicks off the same jobs

--- src/main/java/org/atos/commutermap/network/service/ConfigurationEndpoint.java
+++ src/main/java/org/atos/commutermap/network/service/ConfigurationEndpoint.java
@@ -1,50 +1,26 @@
 package org.atos.commutermap.network.service;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParametersBuilder;
+import org.atos.commutermap.batch.config.SchedulerConfig;
 import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Date;
 
 @Controller
 public class ConfigurationEndpoint {
 
     @Autowired
-    private Job mavJob;
+    private SchedulerConfig schedulerConfig;
 
-    @Autowired
-    private JobRepository jobRepository;
-
-    private SimpleJobLauncher launcher;
-
-    public JobLauncher getLauncher() {
-        if (launcher == null) {
-            launcher = new SimpleJobLauncher();
-            launcher.setJobRepository(jobRepository);
-            launcher.setTaskExecutor(new SimpleAsyncTaskExecutor("batch"));
-        }
-        return launcher;
-    }
-
-    @GetMapping("config/startJob")
+    @GetMapping("config/startJobs")
     @ResponseBody
     public String startJobNow() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
-        JobExecution jobExecution = getLauncher().run(mavJob,
-                new JobParametersBuilder()
-                        .addDate("startedAt", new Date())
-                        .toJobParameters());
-        return jobExecution.toString();
+        schedulerConfig.scheduleJobsEveryNight();
+
+        return "OK";
     }
 }
