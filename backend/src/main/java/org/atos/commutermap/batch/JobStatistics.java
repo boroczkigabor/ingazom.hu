0ba0e3eac391719ac1e075eac8d9c4371f54730b
Gabor Boroczki <gabor_boroczki@epam.com>
2020-04-27 16:16:40 +0200

    Implemented basic statistics handling logic

---
+++ backend/src/main/java/org/atos/commutermap/batch/JobStatistics.java
@@ -0,0 +1,60 @@
+package org.atos.commutermap.batch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class JobStatistics {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JobStatistics.class);
+
+    private static class JobStatistic {
+        private final AtomicInteger numberOfRoutesTotal = new AtomicInteger(0);
+        private final AtomicInteger numberOfMavRequests = new AtomicInteger(0);
+        private final AtomicInteger filteredDueToBeingTooFar = new AtomicInteger(0);
+        private final AtomicInteger filteredDueToLongDuration = new AtomicInteger(0);
+        private final AtomicInteger alreadyUpToDate = new AtomicInteger(0);
+    }
+
+    private static final JobStatistic jobStatistic = new JobStatistic();
+
+    public static void route() {
+        jobStatistic.numberOfRoutesTotal.incrementAndGet();
+    }
+
+    public static void mavRequestIssued() {
+        jobStatistic.numberOfMavRequests.incrementAndGet();
+    }
+
+    public static void filteredAsBeingTooFar() {
+        jobStatistic.filteredDueToBeingTooFar.incrementAndGet();
+    }
+
+    public static void filteredDueToTooLongTravelling() {
+        jobStatistic.filteredDueToLongDuration.incrementAndGet();
+    }
+
+    public static void alreadyUpToDate() {
+        jobStatistic.alreadyUpToDate.incrementAndGet();
+    }
+
+    public static void reset() {
+        jobStatistic.alreadyUpToDate.set(0);
+        jobStatistic.filteredDueToLongDuration.set(0);
+        jobStatistic.filteredDueToBeingTooFar.set(0);
+        jobStatistic.numberOfMavRequests.set(0);
+        jobStatistic.numberOfRoutesTotal.set(0);
+    }
+
+    public static void printStatistics() {
+        LOGGER.info("Job completed with the following statistics:\n" +
+                    "Number of total routes:                {}" +
+                    "Number of MAV requests issued:         {}" +
+                    "Number of routes already up to date:   {}" +
+                    "Number of destinations too far(dist):  {}" +
+                    "Number of destinations too far(time):  {}",
+                jobStatistic.numberOfRoutesTotal, jobStatistic.numberOfMavRequests, jobStatistic.alreadyUpToDate,
+                jobStatistic.filteredDueToBeingTooFar, jobStatistic.filteredDueToLongDuration);
+    }
+}
--- backend/src/main/java/org/atos/commutermap/batch/steps/MarkFarAwayRoutesProcessor.java
+++ backend/src/main/java/org/atos/commutermap/batch/steps/MarkFarAwayRoutesProcessor.java
@@ -1,5 +1,6 @@
 package org.atos.commutermap.batch.steps;
 
+import org.atos.commutermap.batch.JobStatistics;
 import org.atos.commutermap.batch.Util;
 import org.atos.commutermap.dao.model.Route;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class MarkFarAwayRoutesProcessor extends StepExecutionAware implements It
         Long maxDurationInMinutes = stepExecution.getJobExecution().getJobParameters().getLong(Util.FILTER_LONGER_THAN_KEY);
         if (item.duration == null || item.duration.toMinutes() > maxDurationInMinutes) {
             LoggerFactory.getLogger(getClass()).info("Marking {} as far away because duration is greater than maximum: {} > {}", item.destinationStation.name, item.duration == null ? null : item.duration.toMinutes(), maxDurationInMinutes);
+            JobStatistics.filteredDueToTooLongTravelling();
             item.markFarAway();
         }
 
