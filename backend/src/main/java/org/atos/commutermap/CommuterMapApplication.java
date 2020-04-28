329e5dbaa23b1a7e0d084ada7d8bb37c830eae0f
Gabor Boroczki <gabor_boroczki@epam.com>
2020-04-28 09:41:24 +0200

    Added support for overriding configurations

--- backend/src/main/java/org/atos/commutermap/CommuterMapApplication.java
+++ backend/src/main/java/org/atos/commutermap/CommuterMapApplication.java
@@ -2,7 +2,12 @@ package org.atos.commutermap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource(
+        value = "/var/app/override.properties",
+        ignoreResourceNotFound = true
+)
 @SpringBootApplication
 public class CommuterMapApplication {
 
