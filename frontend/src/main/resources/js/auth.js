27e53eda6bc9fa76e7c121a44d62d9041ec531ab
Gabor Boroczki <gabor_boroczki@epam.com>
2020-04-30 10:28:18 +0200

    Added a Facebook login option (WIP)

--- frontend/src/main/resources/js/auth.js
+++ frontend/src/main/resources/js/auth.js
@@ -1,6 +1,5 @@
 function googleSignedIn(googleUser) {
-    var avatarImg = document.getElementById('avatarImg');
-    avatarImg.src = googleUser.getBasicProfile().getImageUrl();
+    setAvatarImg(googleUser.getBasicProfile().getImageUrl());
 
     config.token_id = googleUser.getAuthResponse().id_token;
     config.oauth_provider = 'Google';
@@ -19,10 +18,29 @@ function googleSignOut() {
 }
 
 function resetAuth() {
-    var avatarImg = document.getElementById('avatarImg');
-
     config.token_id = undefined;
     config.oauth_provider = undefined;
     document.getElementById('avatar_a').onclick = function() { showModal('authModal'); };
-    avatarImg.src = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y';
+    setAvatarImg('https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y');
+}
+
+function setAvatarImg(imageUrl) {
+    var avatarImg = document.getElementById('avatarImg');
+    avatarImg.src = imageUrl;
+}
+
+function facebookStatusChangeCallback(response) {
+    if (response.status === 'connected') {
+        config.token_id = response.authResponse.accessToken;
+        config.oauth_provider = 'Facebook';
+        setAvatarImg('https://graph.facebook.com/' + response.authResponse.userID + '/picture');
+        hideModal('authModal');
+    }
+}
+
+function facebookLogOut() {
+    FB.logout(function(response) {
+        console.log('User signed out.');
+        resetAuth();
+    })
 }
