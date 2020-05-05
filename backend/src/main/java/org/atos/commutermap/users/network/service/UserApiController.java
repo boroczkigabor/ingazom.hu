26f454c063ae1f8f50465edb5aa78b84abe1a3e4
Gabor Boroczki <gabor_boroczki@epam.com>
2020-05-05 16:22:30 +0200

    Added some logging

--- backend/src/main/java/org/atos/commutermap/users/network/service/UserApiController.java
+++ backend/src/main/java/org/atos/commutermap/users/network/service/UserApiController.java
@@ -4,6 +4,8 @@ import org.atos.commutermap.network.service.UsersApi;
 import org.atos.commutermap.network.service.model.PlainUser;
 import org.atos.commutermap.users.dao.ApplicationUserRepository;
 import org.atos.commutermap.users.model.ApplicationUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ import java.util.stream.StreamSupport;
 
 @RestController
 public class UserApiController implements org.atos.commutermap.network.service.UserApi, UsersApi {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserApiController.class);
 
     @Autowired
     private ApplicationUserRepository userRepository;
@@ -31,10 +34,12 @@ public class UserApiController implements org.atos.commutermap.network.service.U
 
         Optional<ApplicationUser> userByEmail = userRepository.findByEmail(applicationUser.email);
         if (userByEmail.isPresent()) {
+            LOGGER.debug("User is already in the system, updating tokens...");
             userByEmail.get().accessTokens.putAll(applicationUser.accessTokens);
             return ResponseEntity.ok().build();
         } else {
             ApplicationUser createdUser = userRepository.save(applicationUser);
+            LOGGER.info("Registered a new user {}", createdUser.toPlainUser());
             return ResponseEntity.created(
                     ServletUriComponentsBuilder.fromCurrentContextPath()
                             .path("user/{id}")
