e652a8a9324df2e9be58a4618c7aabe016679a0c
Gabor Boroczki <gabor_boroczki@epam.com>
2020-05-07 10:59:46 +0200

    Included Spring Security with CSRF protection disabled (WIP) + fixed the URLs on the UI side to not have double slashes

--- frontend/src/main/resources/js/initMap.js
+++ frontend/src/main/resources/js/initMap.js
@@ -13,7 +13,7 @@ let baseUrl = config.baseUrl;
     }
 
     function getBaseStations() {
-        return fetch(baseUrl + '/baseStations/')
+        return fetch(baseUrl + 'baseStations')
             .then(response => response.json())
             .then(data => {
                 data.forEach( function(item) {
@@ -42,7 +42,7 @@ let baseUrl = config.baseUrl;
               });
         let minimumMinute = '9999';
 
-        fetch(baseUrl + '/destinationsForMap/' + baseStation.id)
+        fetch(baseUrl + 'destinationsForMap/' + baseStation.id)
               .then(response=>response.json())
               .then(data => {
                   data.forEach(function(item) {
